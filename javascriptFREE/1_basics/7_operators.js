29140a6422a61218d99f53f6fd1a36c3d5e4177e
JavaScriptFREE <wjsrhkdtn123@naver.com>
2024-12-30 23:27:35 +0900

    operators
    operators에 대해서 공부

--- javascriptFREE/1_basics/7_operators.js
+++ javascriptFREE/1_basics/7_operators.js
@@ -82,3 +82,139 @@ console.log(number); // 100
 
 number += 10;
 console.log(number); // 110
+
+number -= 10;
+console.log(number); // 100
+
+number *= 10;
+console.log(number); // 1000
+
+number /= 10;
+console.log(number); // 100
+
+number %= 10;
+console.log(number); // 0
+
+/**
+ * 비교 연산자
+ * 
+ * 1) 값의 비교
+ * 2) 값과 타입의 비교
+ */
+
+console.log(5 == 5); // true
+console.log(5 == '5'); // true
+console.log(0 == ''); // true
+console.log(true == 1); // true
+console.log(false == 0); // true
+console.log(true == '1'); // true
+
+console.log(5 === 5); // true
+console.log(5 === '5'); // false
+console.log(0 == ''); // true
+console.log(true === 1); // false
+console.log(false === 0); // false
+console.log(true === '1'); // false
+
+console.log('---------------------');
+
+console.log(5 != 5); // false
+console.log(5 != '5'); // false
+console.log(0 != ''); // false
+console.log(true != 1); // false
+console.log(false != 0); // false
+console.log(true != '1'); // false
+
+console.log('---------------------');
+
+console.log(5 !== 5); // false
+console.log(5 !== '5'); // true
+console.log(0 !== ''); // true
+console.log(true !== 1); // true
+console.log(false !== 0); // true
+console.log(true !== '1'); // true
+
+console.log('---------------------');
+
+/**
+ * 대소 관계 비교 연산자
+ */
+console.log(5 > 3); // true
+console.log(5 < 3); // false
+console.log(5 >= 3); // true
+console.log(5 <= 3); // false
+
+/**
+  * 삼항 조건 연산자 (ternary operator)
+ */
+console.log(10 > 0 ? '10이 0보다 크다' : '10이 0보다 작다'); // 10이 0보다 크다
+console.log('---------------------');
+/**
+ * 논리 연산자
+ * 
+ * 1) && (and)
+ * 2) || (or)
+ */
+
+// && (and) 조건은 모두 true 일 때만 true
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+console.log('---------------------');
+
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+console.log('---------------------');
+console.log(10 > 1 && 20 > 2); // true
+console.log(10 > 1 && 20 < 2); // false
+console.log(10 < 1 && 20 > 2); // false
+console.log(10 < 1 && 20 < 2); // false
+console.log('---------------------');
+console.log(10 > 1 || 20 > 2); // true
+console.log(10 > 1 || 20 < 2); // true
+console.log(10 < 1 || 20 > 2); // true
+console.log(10 < 1 || 20 < 2); // false
+
+/**
+ * 단축평가 (short-circuit evaluation)
+ * 
+ * &&를 사용했을떄 좌측이 true면 우측 값 반환
+ * &&를 사용했을때 좌측이 false면 좌측 값 반환
+ * ||를 사용했을때 좌측이 true면 좌측 값 반환
+ * ||를 사용했을때 좌측이 false면 우측 값 반환
+ */
+console.log('---------------------');
+console.log(true || '아이브'); // true
+console.log(false || '아이브'); // 아이브
+console.log(true && '아이브'); // 아이브
+console.log(false && '아이브'); // false
+
+console.log(true && true && '아이브'); // 아이브
+console.log(true && false && '아이브'); // false
+
+/**
+ * 지수 연산자
+ */
+console.log('---------------------');
+console.log(2 ** 3); // 8
+console.log(10 ** 3); // 1000
+
+/**
+ * null 연산자
+ */
+let name;
+console.log(name);
+
+name = name ?? '코드팩토리'; // name이 null이나 undefined면 '코드팩토리'를 할당
+console.log.name;
+
+name = name ?? '아이브';
+console.log(name); // 코드팩토리
+
+let name2;
+name2 ??= '코드팩토리';
+console.log(name2); // 코드팩토리
